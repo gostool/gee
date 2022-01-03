38804ba007e7fe260808fa82389cd13908aa92df
tphyhFighting <2363176358@qq.com>
2022-01-03 17:29:18 +0800

    add: v0.8

--- gee_test.go
+++ gee_test.go
@@ -75,6 +75,19 @@ func TestGee(t *testing.T) {
 	r.Run(":8000")
 }
 
+func TestGeeError(t *testing.T) {
+	r := Default()
+	r.GET("/", func(c *Context) {
+		c.String(http.StatusOK, "Hello Geektutu\n")
+	})
+	// index out of range for testing Recovery()
+	r.GET("/panic", func(c *Context) {
+		names := []string{"geektutu"}
+		c.String(http.StatusOK, names[100])
+	})
+	r.Run(":8000")
+}
+
 func newTestRouter() *router {
 	r := newRouter()
 	r.addRoute("GET", "/", nil)
